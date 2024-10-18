f64f9ee97a3821d45df648e7a586db022e91737e
kazantsevalexander <kazantsevalexander@gmail.com>
2024-10-18 21:05:09 +0300

    fin

--- main.py
+++ main.py
@@ -9,13 +9,16 @@ def find_paragraphs():
         sel = input('Выберите действие:\n1 - листать параграфы текущей статьи\n2 - перейти на одну из связанных страниц\n3 - выйти из программы\n')
         if sel == '1':
             paragraphs = browser.find_elements(By.TAG_NAME, "p")
-            print(paragraphs)
             for paragraph in paragraphs:
                 print(paragraph.text)
                 input()
 
         if sel == '2':
-            pass
+            links = []
+            for element in browser.find_elements(By.TAG_NAME, "a"):
+                link = element.get_attribute("href")
+                links.append(link)
+            browser.get(random.choice(links))
 
         if sel == '3':
             exit()
@@ -25,10 +28,9 @@ text_find = input('Введите поисковой запрос: ')
 browser = webdriver.Firefox()
 browser.get('https://www.wikipedia.org/')
 
-assert 'Википедия' in browser.title
+assert 'Wikipedia' in browser.title
 search_box = browser.find_element(By.ID, 'searchInput')
 search_box.send_keys(text_find)
 search_box.send_keys(Keys.RETURN)
-time.sleep(100)
 
 find_paragraphs()
